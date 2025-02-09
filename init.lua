99aa2e591130652996f3a8a1d0d84ce4d05b8c3c
Fepozopo <fepozopo.f39@gmail.com>
2025-02-08 19:43:01 -0800

    Updated init.lua to add formatters for Go and Python

--- init.lua
+++ init.lua
@@ -679,6 +679,8 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'goimports', -- Used to format Go code
+        'black', -- Used to format Python code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -736,6 +738,8 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        go = { 'goimports' },
+        python = { 'black' },
       },
     },
   },
